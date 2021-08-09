9c8d22959b9d3b09689c7e27643e1f6d2a68c5a9
edwinthekkodan <84464950+edwinthekkodan@users.noreply.github.com>
2021-08-09 11:29:10 +0530

    Update FirstClass.java

--- FirstClass.java
+++ FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
-			System.out.println("Hello World");
+			System.out.println("Hello This World");
 		}
 	}
 }
